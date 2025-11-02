13965b70bdca0e51d9d570ddebe6be3fb2569bca
raihandykampus <133252328+Redsfandys@users.noreply.github.com>
2025-11-02 13:17:08 +0700

    pertemuan9

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+</head>
+<body>
+    <h1>Profil Saya</h1>
+
+    <p>Halo, {{ Auth::user()->name }}!</p>
+
+    <p>Email kamu adalah {{ Auth::user()->email }}</p>
+    <p>Peran kamu adalah {{ Auth::user()->role }}</p>
+
+    <br>
+    <a href="{{ route('dashboard') }}">Kembali ke Dashboard</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,9 +12,31 @@ Route::get('/about', function () {
     return view('about');
 });
 
-Route::get('/login', [AuthController::class,'showLoginForm'])->name('login');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class,'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class,'register']);
 Route::post('/logout', [AuthController::class,'logout'])->name('logout');
-Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
+
+Route::get('/dashboard', [AuthController::class, 'dashboard'])->middleware(['auth']);
+Route::get('/dashboard', [AuthController::class, 'dashboard'])->middleware(['auth'])->name('dashboard');
+
+// LATIHAN 1: route profile
+Route::get('/profile', function() {
+    return view('profile');
+})->middleware(['auth'])->name('profile');
+
+Route::get('/hello', function() {
+    return "Halo, ini halaman percobaan route!";
+});
+
+Route::get('/jobs', [JobController::class, 'index']);
+
+Route::get('/admin', function() {
+    return "Halo Admin! Ini Halaman Khusus Admin.";
+})->middleware(['auth', 'isAdmin']);
+
+// LATIHAN 2: Route Untuk kelola jobs (admin only)
+Route::get('/admin/jobs', function() {
+    return "Halaman Kelola Lowongan Kerja (Khusus Admin)";
+})->middleware(['auth', 'isAdmin']);
